9f1b9030c9fb84209295079ec7b2122679756a01
Moskotlinov Roman <rom_iras@mail.ru>
2020-03-26 08:59:37 +0300

    Готова 2 задача дз 10 урок

--- dz10.sql
+++ dz10.sql
@@ -1,12 +1,13 @@
- SELECT DISTINCT
+-- DZ 
+ SELECT DISTINCT 
  communities.name as 'Group_Name',
- COUNT(users.id) OVER () / (SELECT COUNT(communities.id ) from communities) as 'AVG_USERS_ON_GROUP',
+ COUNT(communities_users.user_id ) OVER () / (SELECT COUNT(communities.id ) from communities) as 'AVG_USERS_ON_GROUP',
  FIRST_VALUE(profiles.user_id ) OVER (PARTITION BY communities_users.community_id  ORDER BY profiles.birthday) as 'MAX_AGE_USER_ID',
  FIRST_VALUE(profiles.user_id ) OVER (PARTITION BY communities_users.community_id  ORDER BY profiles.birthday desc) as 'MIN_AGE_USER_ID',
  COUNT(communities_users.user_id) OVER (PARTITION BY communities_users.community_id) as 'total_users_on_group',
  (SELECT COUNT(users.id) from users)  as 'total_users_on_system',
  COUNT(communities_users.user_id) OVER (PARTITION BY communities_users.community_id) / (SELECT COUNT(users.id) from users) * 100 as 'Proc'
  	FROM (communities_users
- 			INNER JOIN communities ON communities_users.community_id = communities.id
+ 			INNER JOIN communities ON communities_users.community_id = communities.id 
  			INNER JOIN profiles    ON communities_users.user_id = profiles.user_id
  			LEFT JOIN users       ON communities_users.user_id = users.id ) order by Proc DESC ;
